668382462d29f9281db33436a46ff1ddff87974f
Yash Pandey <knowledge86400@gmail.com>
2021-12-15 11:14:42 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// This is being used when your jquery is not properly placed
+// $(document).ready(function () {
+//   $("h1").css("color", "red");
+// });
+
+
+
+// $("h1").css("color", "green");
+$("h1").addClass("big-class");
+// $("h1").removeClass("big-class");
+
+// $("button").text("<em>Don't Click Me</em>");
+$("button").html("<em>Don't Click Me</em>");
+// $("a").attr("href", "https://www.yahoo.com");
